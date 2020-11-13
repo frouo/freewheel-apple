b1a1709f08cd4a35024595a04c3bf635eb7d2b98
François Rouault <francois.rouault@cocoricostudio.com>
2020-11-13 18:30:23 +0100

    Create a workspace with an iOS and tvOS Example app

---
+++ Example/Example-tvOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example-tvOS
+//
+//  Created by François Rouault on 13/11/2020.
+//  Copyright © 2020 François Rouault. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Example/Example/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by François Rouault on 13/11/2020.
+//  Copyright © 2020 François Rouault. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+}
+
