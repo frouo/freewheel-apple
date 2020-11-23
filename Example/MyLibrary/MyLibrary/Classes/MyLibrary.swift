f16eb0660e2d061a221ed0b1142e75d3294e8f03
François Rouault <francois.rouault@cocoricostudio.com>
2020-11-23 16:56:57 +0100

    Create an example pod MyLibrary

---
+++ Example/MyLibrary/MyLibrary/Classes/MyLibrary.swift
@@ -0,0 +1,14 @@
+//
+//  MyLibrary.swift
+//  MyLibrary-iOS
+//
+//  Created by François Rouault on 23/11/2020.
+//
+
+import Foundation
+
+public enum MyLibrary {
+  public func sayHello() {
+    print("hello!")
+  }
+}
